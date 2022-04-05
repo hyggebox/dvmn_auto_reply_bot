f6d37512259919824a168a86833de2b9e78a5e65
Anastasia <asya_apto@mail.ru>
2022-04-05 14:24:22 +0300

    Add DialogFlow to VK bot

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import logging
 import random
+import vk_api as vk
 
 from environs import Env
 from google.cloud import dialogflow
-from telegram import Update
-from telegram.ext import (CallbackContext,
-                          CommandHandler,
-                          Filters,
-                          MessageHandler,
-                          Updater)
-
-import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 
+
 LANGUAGE_CODE = "ru-RU"
-
-
 
 
 def detect_intent_texts(project_id, session_id, text, language_code):
@@ -37,10 +29,13 @@ def detect_intent_texts(project_id, session_id, text, language_code):
     return response.query_result.fulfillment_text
 
 
-def echo(event, vk_api):
+def echo(event, vk_api, project_id, session_id):
+    bots_answer = detect_intent_texts(project_id, session_id,
+                                      event.text, LANGUAGE_CODE)
+
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
+        message=bots_answer,
         random_id=random.randint(1,1000)
     )
 
@@ -54,6 +49,8 @@ if __name__ == '__main__':
     env.read_env()
 
     vk_group_token = env.str('VK_TOKEN')
+    project_id = env.str('PROJECT_ID')
+    session_id = env.str('SESSION_ID')
 
     vk_session = vk.VkApi(token=vk_group_token)
     vk_api = vk_session.get_api()
@@ -61,20 +58,11 @@ if __name__ == '__main__':
 
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            echo(event, vk_api)
-
-    # for event in longpoll.listen():
-    #     if event.type == VkEventType.MESSAGE_NEW:
-    #         print('Новое сообщение:')
-    #         if event.to_me:
-    #             print('Для меня от: ', event.user_id)
-    #         else:
-    #             print('От меня для: ', event.user_id)
-    #         print('Текст:', event.text)
+            echo(event, vk_api, project_id, session_id)
 
 
-    project_id = env.str('PROJECT_ID')
-    session_id = env.str('SESSION_ID')
+
+
 
 
 
