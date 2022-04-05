ba6a0989c62d489911cd603af644b16e854fc956
Anastasia <asya_apto@mail.ru>
2022-04-05 14:09:05 +0300

    Add echobot for VK

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import random
 
 from environs import Env
 from google.cloud import dialogflow
@@ -9,7 +10,7 @@ from telegram.ext import (CallbackContext,
                           MessageHandler,
                           Updater)
 
-import vk_api
+import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 LANGUAGE_CODE = "ru-RU"
@@ -36,6 +37,14 @@ def detect_intent_texts(project_id, session_id, text, language_code):
     return response.query_result.fulfillment_text
 
 
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1,1000)
+    )
+
+
 if __name__ == '__main__':
     logging.basicConfig(
         format='%(levelname)s: %(name)s - %(message)s - %(asctime)s',
@@ -46,17 +55,22 @@ if __name__ == '__main__':
 
     vk_group_token = env.str('VK_TOKEN')
 
-    vk_session = vk_api.VkApi(token=vk_group_token)
+    vk_session = vk.VkApi(token=vk_group_token)
+    vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            print('Новое сообщение:')
-            if event.to_me:
-                print('Для меня от: ', event.user_id)
-            else:
-                print('От меня для: ', event.user_id)
-            print('Текст:', event.text)
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, vk_api)
+
+    # for event in longpoll.listen():
+    #     if event.type == VkEventType.MESSAGE_NEW:
+    #         print('Новое сообщение:')
+    #         if event.to_me:
+    #             print('Для меня от: ', event.user_id)
+    #         else:
+    #             print('От меня для: ', event.user_id)
+    #         print('Текст:', event.text)
 
 
     project_id = env.str('PROJECT_ID')
