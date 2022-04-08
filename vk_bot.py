0412a23cb0826bc81771619a8ef43dca9774d4cb
Anastasia <asya_apto@mail.ru>
2022-04-08 15:47:56 +0300

    Rename get_reply() send_reply_msg()

--- vk_bot.py
+++ vk_bot.py
@@ -26,7 +26,7 @@ class VKLogsHandler(logging.Handler):
                                   random_id=random.randint(1,1000))
 
 
-def get_reply(event, vk_api, project_id, session_id):
+def send_reply_msg(event, vk_api, project_id, session_id):
     bots_answer = get_reply_msg(project_id, session_id, event.text)
     if bots_answer:
         vk_api.messages.send(
@@ -58,7 +58,7 @@ if __name__ == '__main__':
             for event in longpoll.listen():
 
                 if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                    get_reply(event, vk_api, project_id, session_id)
+                    send_reply_msg(event, vk_api, project_id, session_id)
 
         except Exception as err:
             logger.exception(f"⚠ Ошибка бота:\n\n {err}")
